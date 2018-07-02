209cdc05bb72078b2300a2bc94ac6f6ce1682e4c
Nils Gawlik <nilsgawlik@gmx.de>
2018-07-02 15:10:02 +0200

    Implement basic 2D steering

---
+++ Assets/Scripts/Kart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kart : MonoBehaviour {
+	public float maxAcceleration = 6f;
+	public float maxSpeed = 16f;
+	public float minSpeed = -4f;
+	public float friction = 0.01f;
+	public float maxSteering = 45f;
+	public AnimationCurve speedToSteeringRatio;
+
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		KartController kc = GetComponent<KartController>();
+		
+		velocity += kc.acceleration * Time.fixedDeltaTime * maxAcceleration 
+			* transform.forward;
+
+		float forwardSpeed = Vector3.Dot(velocity, transform.forward);
+		forwardSpeed *= 1 - friction;
+		forwardSpeed = Mathf.Clamp(forwardSpeed, minSpeed, maxSpeed);
+
+		velocity = forwardSpeed * transform.forward;
+
+		transform.position += velocity;
+		
+        float angle = kc.steering * Time.fixedDeltaTime * maxSteering * speedToSteeringRatio.Evaluate(forwardSpeed / maxSpeed);
+        transform.Rotate(transform.up, angle);
+	}
+}
--- Assets/Scripts/KartController.cs
+++ Assets/Scripts/KartController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class KartController : MonoBehaviour {
 
+	public float steering = 0;
+	public float acceleration = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,7 @@ public class KartController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		steering = Input.GetAxis("Horizontal");
+		acceleration = Input.GetAxis("Vertical");
 	}
 }
