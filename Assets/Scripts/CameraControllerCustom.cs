dc5e69883b77e12a8ac0659979504fce5ffa5f01
Nils Gawlik <nilsgawlik@gmx.de>
2018-07-04 11:09:42 +0200

    Implement ray based wall snapping and add more test geometry

---
+++ Assets/Scripts/CameraControllerCustom.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControllerCustom : MonoBehaviour
+{
+	public float lookSpeed = 4f;
+	public GameObject following;
+	
+	Transform followTransform;
+	float initialDistance;
+	Vector3 initialPos;
+
+	private void Start() {
+		following = following != null? following : GameObject.Find("HappyPoint");
+		// Debug.Log("Found the cam target object:");
+		// Debug.Log(following);
+		followTransform = following.transform;
+
+		initialDistance = CalcPlaneDistance(transform.position, followTransform.position);
+		initialPos = (transform.position - followTransform.position);
+	}
+
+	void FixedUpdate() {
+		
+		try
+		{
+			// rotate camera around the target
+			Vector3 localPos = followTransform.InverseTransformPoint(transform.position);
+			localPos = Quaternion.Euler(Input.GetAxis("LookY") * lookSpeed, Input.GetAxis("LookX") * lookSpeed, 0f) * localPos;
+			transform.position = followTransform.TransformPoint(localPos);
+		}
+		catch (System.ArgumentException)
+		{
+			// axis not set up, do nothing
+		}
+
+		// normal follow behaviour
+		Vector3 diffVector = transform.position - followTransform.position;
+		diffVector = new Vector3(diffVector.x, 0, diffVector.z);
+		float currentDistance = diffVector.magnitude;
+		float nextDistance = (currentDistance + initialDistance) / 2;
+
+		transform.position = followTransform.position + diffVector.normalized * nextDistance;
+		transform.position = new Vector3(
+			transform.position.x, 
+			followTransform.position.y + initialPos.y, 
+			transform.position.z
+		);
+
+		transform.rotation = Quaternion.LookRotation(-diffVector + new Vector3(0, -initialPos.y, 0));	
+	}
+
+	private void OnDrawGizmos() {
+		if (followTransform != null)
+			Gizmos.DrawSphere(followTransform.position, .1f);
+	}
+	
+	private float CalcPlaneDistance(Vector3 a, Vector3 b) {
+		Vector3 flatA = new Vector3(a.x, 0f, a.z);
+		Vector3 flatB = new Vector3(b.x, 0f, b.z);
+		return (flatA - flatB).magnitude;
+	}
+}
--- Assets/Scripts/Kart.cs
+++ Assets/Scripts/Kart.cs
@@ -9,6 +9,15 @@ public class Kart : MonoBehaviour {
 	public float friction = 0.01f;
 	public float maxSteering = 45f;
 	public AnimationCurve speedToSteeringRatio;
+	public float gravity = 1f;
+
+	[Header("Ray based rotation settings")]
+	public Vector3 localFrontCheck = Vector3.forward;
+	public Vector3 localBackCheck = Vector3.back;
+	public float rayStartHeight = 1f;
+	public float rayLength = 2f;
+	public float floorOffset = .2f;
+	public float maximuChangeAnglePerSecond = 90;
 
 	private Vector3 velocity;
 
@@ -19,26 +28,42 @@ public class Kart : MonoBehaviour {
 	
 	void FixedUpdate () {
 		KartController kc = GetComponent<KartController>();
-		Rigidbody rb = GetComponent<Rigidbody>();
 		
-		velocity += kc.acceleration * Time.fixedDeltaTime * maxAcceleration 
+		velocity += Vector3.down * gravity * Time.fixedDeltaTime;
+
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position + transform.up * rayStartHeight, -transform.up, out hit, rayLength)) {
+			Quaternion rotation = Quaternion.LookRotation(hit.normal, transform.right)
+				* Quaternion.Euler(180, -90, -90);
+			// if(Quaternion.Angle(transform.rotation, rotation) < maximuChangeAnglePerSecond * Time.fixedDeltaTime) {
+			transform.rotation = rotation;
+			// }
+
+			velocity += kc.acceleration * Time.fixedDeltaTime * maxAcceleration 
 			* transform.forward;
 
-		float forwardSpeed = Vector3.Dot(velocity, transform.forward);
-		forwardSpeed *= 1 - friction;
-		forwardSpeed = Mathf.Clamp(forwardSpeed, minSpeed, maxSpeed);
+			float forwardSpeed = Vector3.Dot(velocity, transform.forward);
+			forwardSpeed *= 1 - friction;
+			forwardSpeed = Mathf.Clamp(forwardSpeed, minSpeed, maxSpeed);
 
-		velocity = forwardSpeed * transform.forward;
+			velocity = forwardSpeed * transform.forward;
 
-		// transform.position += velocity;
-		rb.MovePosition(rb.position + velocity);
+			transform.position = hit.point + hit.normal * floorOffset;
+			velocity -= Vector3.Dot(velocity, hit.normal) * hit.normal;
+		}
 		
-        float angle = kc.steering * Time.fixedDeltaTime * maxSteering * speedToSteeringRatio.Evaluate(forwardSpeed / maxSpeed);
-        // transform.Rotate();
-		rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(angle, transform.up));
+        float angle = kc.steering * Time.fixedDeltaTime * maxSteering * speedToSteeringRatio.Evaluate(Vector3.Dot(velocity, transform.forward) / maxSpeed);
+        transform.Rotate(transform.up, angle);
+		// rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(angle, transform.up));
+		
+		transform.position += velocity;
+		// rb.MovePosition(rb.position + velocity);
 	}
 	
-	private void OnCollisionEnter(Collision other) {
-		// velocity = -velocity;
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere(transform.TransformPoint(localFrontCheck), .2f);
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(transform.TransformPoint(localBackCheck), .2f);
 	}
 }
